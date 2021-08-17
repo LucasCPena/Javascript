d4ca25da03695194f3327dfdec75aabb57cfd5a5
Lucas <lucascicalap@hotmail.com>
2021-08-17 10:53:12 -0300

    att

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var nome = "Lucas Cicala Pena ";
 var idade = 19;
 var idade2 = 20;
+var frase = "japão é foda"
 //alert( nome + "tem " + idade + "anos");
-console.log()
+console.log(nome);
+console.log(idade +idade2);
+//console.log(frase.replace("japão","Brasil"));
+//alert(frase.replace("japão","Brasil"));
+console.log(frase.toLowerCase());
